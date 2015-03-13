e34864e5508797331e24cab05429555907b180be
Mark Macdonald <mjemacdonald@gmail.com>
2015-03-13 16:07:38 -0700

    (feat) Adds codemirror initialization and EditorController

--- client/app/components/editor/editorController.js
+++ client/app/components/editor/editorController.js
@@ -1,2 +1,24 @@
 angular.module('RecallJS')
-  .controller('EditorController', function(){});
+  .controller('EditorController', function($scope){
+    var egInput = 'GETINPUT';
+    var egOutput = 'GETOUTPUT';
+    var prompt = 'GETPROMPT';
+    var funcName = 'GETFUNCNAME';
+
+    CodeMirror(document.body, {
+      value: "egInput",
+      mode:  "javascript",
+      linenumbers: true
+    });
+    CodeMirror(document.body, {
+      value: "egOutput",
+      mode:  "javascript",
+      linenumbers: true
+    });
+    CodeMirror(document.body, {
+      value: "functionName",
+      mode:  "javascript",
+      // theme: "solarized",
+      linenumbers: true
+    });
+  });
