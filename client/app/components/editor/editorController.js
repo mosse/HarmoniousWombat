61dbece6e26fb1b54c765b1119379b3e8746062e
Mark Macdonald <mjemacdonald@gmail.com>
2015-03-13 16:35:19 -0700

    (feat) Sets variables for EditorController

--- client/app/components/editor/editorController.js
+++ client/app/components/editor/editorController.js
@@ -1,22 +1,30 @@
 angular.module('RecallJS')
-  .controller('EditorController', function($scope){
-    var egInput = 'GETINPUT';
-    var egOutput = 'GETOUTPUT';
-    var prompt = 'GETPROMPT';
-    var funcName = 'GETFUNCNAME';
+  .controller('EditorController', function($scope/*,LearningAlgo*/){
+    // var problems = LearningAlgo.getProblems;
+    var problems = [ { 
+      title: 'Add Two Numbers',
+      functionName: 'addNums',
+      examples: [ 'addNums(10, 2) === 12 // returns true' ],
+      attempts: [ [Object], [Object] ],
+      tests: [ [Object], [Object] ] },
+    { title: 'Subtract Two Numbers',
+      functionName: 'subtractNums',
+      examples: [ 'subtractNums(10, 2) === 8 // returns true' ],
+      attempts: [ [Object], [Object] ],
+      tests: [ [Object], [Object] ] } ]
+
+    var example = problems[0].examples[0];
+    var prompt = problems[0].title;
+    var funcName = problems[0].functionName;
 
     CodeMirror(document.body, {
-      value: "egInput",
+      value: example,
       mode:  "javascript",
       linenumbers: true
     });
+  
     CodeMirror(document.body, {
-      value: "egOutput",
-      mode:  "javascript",
-      linenumbers: true
-    });
-    CodeMirror(document.body, {
-      value: "functionName",
+      value: funcName,
       mode:  "javascript",
       // theme: "solarized",
       linenumbers: true
