001e0ddb48c8f562d3bbf8e5aafecaee5c0011fc
Chris Rinaldi <cgrinaldi@gmail.com>
2015-03-16 11:22:28 -0700

    (feat) Create calcAgeWeight
    Function takes in a given problem and calculates the associated
    weight based on how many days it has been since the most recent
    attempt.
    
    Currently, the key assumption is that the most recent attempt
    will be the last item on the attempts array.

--- client/app/shared/learningAlgoService.js
+++ client/app/shared/learningAlgoService.js
@@ -13,6 +13,16 @@ function LearningAlgo(UserData) {
     return data.problems;
   }
 
+  function calcAgeWeight(problem) {
+    // identify number of days since last attempt
+    var timeLastAttempt = problem.attempts.slice(-1)[0].timeSubmitted; // assuming last attempt is most recent
+    var currTime = (new Date()).getTime(); // in milliseconds
+    var daysSince = convertMStoDAYS(currTime - timeLastAttempt);
+
+    // calculate weight
+    return Math.pow(2, daysSince/5); // weight will double every five days
+  }                                  // since the last attempt
+
   // helper function to convert milliseconds to days
   function convertMStoDAYS(numMS) {
     return Math.round(numMS / (1000 * 60 * 60 * 24));
