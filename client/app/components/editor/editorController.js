efa5a926479b947589897ef7b72704e08d37d57b
Mark Macdonald <mjemacdonald@gmail.com>
2015-03-16 22:20:50 -0700

    (refactor) Adds ui-codemirror directive

--- client/app/components/editor/editorController.js
+++ client/app/components/editor/editorController.js
@@ -1,3 +1,5 @@
+//TODO: Do we need to inject the CodeMirror dependency directly?
+
 angular.module('RecallJS')
   .controller('EditorController', function($scope, LearningAlgo){
     var problem = LearningAlgo.getProblem();
@@ -9,7 +11,8 @@ angular.module('RecallJS')
     $scope.examples = problem.examples;
     $scope.funcDeclaration = "var " + problem.functionName + " = function(){/*YOUR CODE HERE*/};";
 
-    CodeMirror(document.body, {
-      mode:  "javascript"
-    });
+    // CodeMirror.fromTextArea(editor);
+  // var editor = document.getElementByName("editor");
+  // CodeMirror.fromTextArea(editor);
+  console.log('success?');
   });
