126079a6841fa72324c685429be7bd86b795ca4f
Chris Rinaldi <cgrinaldi@gmail.com>
2015-03-18 14:36:16 -0400

    (feat) Include numTests and details for each test passed
    Currently, details is an object with two keys, correct and incorrect.
    The values associated are simply strings, but we could change this.

--- client/app/shared/codeEval.js
+++ client/app/shared/codeEval.js
@@ -9,6 +9,7 @@ function CodeEval() {
   function run(problem, code) {
     // getting info from the problem
     var tests = problem.tests;
+    var funcName = problem.functionName;
 
     // creating a function object out of user provided code
     code = code.split("=")[1];
@@ -17,18 +18,44 @@ function CodeEval() {
     }
     var submittedFunc = eval("(" + code + ")");
 
-    // iterate through our tests, using user provided function
+    // data to track
     var numCorrect = 0;
+    var correctDetails = [];
+    var incorrectDetails = [];
+
+    // iterate through our tests, using user provided function
     tests.forEach(function(test){
       var args = test.input; // an array of arguments
       var expected = test.output;
       var actual = submittedFunc.apply(null, args);
 
-      // increment number correct based on passing test
+      // add info about passing/failing test
       if (actual === expected) {
         numCorrect++;
+        correctDetails.push(getDetails(funcName, args, expected));
+      } else {
+        incorrectDetails.push(getDetails(funcName, args, expected, actual));
       }
     });
-    return {numCorrect: numCorrect};
+
+    return {
+        numCorrect: numCorrect,
+        numTests: tests.length,
+        details: {
+          correct: correctDetails,
+          incorrect: incorrectDetails
+        }
+      };
+  }
+
+  // TODO: Decide what information we actually want to pass
+  // for the details
+  function getDetails(funcName, args, expected, actual) {
+    if (arguments.length === 3) {
+      return "Correct: " + funcName + "(" + args + ")" +
+             " === " + expected;
+    }
+    return "Incorrect: Expected " + funcName + "(" + args + ")" +
+           " === " + expected + ", not " + actual;
   }
 }
