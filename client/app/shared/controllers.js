94087ebe86be798b50dc6a40151dc09a4f23a948
Chris Rinaldi <cgrinaldi@gmail.com>
2015-03-19 14:22:02 -0400

    (feat) Change login button based on user status
    If the user is logged in, the button will be red and read "Logout".
    Otherwise, it will be green and read "Login".

--- client/app/shared/controllers.js
+++ client/app/shared/controllers.js
@@ -3,8 +3,14 @@ angular.module('RecallJS')
 
 // controller will change the active class on
 // correct link in the navigation
-function HeaderController($scope, $location){
+function HeaderController($scope, $location, Auth){
   $scope.isActive = function(viewLocation) {
     return viewLocation === $location.path();
   };
+
+  $scope.isLoggedIn = function() {
+    return Auth.isAuth();
+  };
+
+  $scope.logout = Auth.signout;
 }
