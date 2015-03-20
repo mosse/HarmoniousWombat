31a162ce5fcdd187ce4ec52575632d4a81ac87bc
Chris Rinaldi <cgrinaldi@gmail.com>
2015-03-20 12:00:36 -0400

    (refactor) Change Examples Accordion to Tests Accordion
    Reason being that for the tests is when we will want inputs
    and their associated outputs.
    
    For the examples, we can just an array of strings.

--- client/app/components/create/createController.js
+++ client/app/components/create/createController.js
@@ -1,6 +1,6 @@
 angular.module('RecallJS')
   .controller('CreateController', function($scope){
-    $scope.examples = [
+    $scope.tests = [
       {input: [1,2,3], output: 6},
       {input: [8,1,3], output: 10},
       {input: [6,3,2], output: 99}
