519146d1ddfa3289bd74a712a8708d0a79369b52
Chris Rinaldi <cgrinaldi@gmail.com>
2015-03-20 11:52:19 -0400

    (feat) Create table of examples with data from controller

--- client/app/components/create/createController.js
+++ client/app/components/create/createController.js
@@ -1,4 +1,8 @@
 angular.module('RecallJS')
   .controller('CreateController', function($scope){
-    // ENTER CONTROLLER CODE HERE
+    $scope.examples = [
+      {input: [1,2,3], output: 6},
+      {input: [8,1,3], output: 10},
+      {input: [6,3,2], output: 99}
+    ];
   });
