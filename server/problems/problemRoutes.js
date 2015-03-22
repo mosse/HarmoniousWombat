deea8842617531784188c7733c4ddd6d6e465b25
garrettjoecox <garrettjcox@gmail.com>
2015-03-22 02:01:34 -0700

    (feat) server-side problem routing and logic

--- server/problems/problemRoutes.js
+++ server/problems/problemRoutes.js
@@ -1,9 +1,37 @@
+var Problem = require('./problemModel');
+var User = require('../users/userModel');
+
 module.exports = function (app) {
-  app.get('/problems', function(req, res, next){
+  app.post('/addOwn', function(req, res, next){
+    User.findOne({username:req.body.username}, function(err, data){
+      data.problems.push(req.body.problem);
+      data.save(data);
+      res.send();
+    });
+  });
+
+  app.post('/removeOwn', function(req, res, next){
+      // WIP
     next();
   });
 
-  app.post('/problems', function(req, res, next){
-    next();
+  app.post('/create', function(req, res, next){
+    Problem.create(req.body, function(err){
+      if (!err){
+        res.send();
+      }
+    });
+  });
+
+  app.get('/getAll', function(req, res){
+    Problem.find({}, function(err, data){
+      res.send(data);
+    });
+  });
+
+  app.get('/getOwn/:username', function(req, res, next){
+    User.findOne({username: req.params.username}, function(err, data){
+      res.send(data.problems);
+    });
   });
 };
