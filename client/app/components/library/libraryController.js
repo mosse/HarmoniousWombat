6c67ffdc37d277e3a2d18cceb73b183253cd1357
Mark Macdonald <mjemacdonald@gmail.com>
2015-03-23 16:30:46 -0700

    (feat) Adds cross/check to Library view

--- client/app/components/library/libraryController.js
+++ client/app/components/library/libraryController.js
@@ -1,13 +1,37 @@
 angular.module('RecallJS')
   .controller('LibraryController', function($window, $scope, Auth, ProblemData, LearningAlgo){
 
+
     ProblemData.getAll()
       .then(function(problems){
         $scope.problems = problems;
       });
 
+    ProblemData.getOwn()
+      .then(function(problems){
+        $scope.dashboardProblems = problems;
+      }); 
+
+
+    $scope.checkDash = function(problem){
+      var inDash = false;
+      
+      $scope.dashboardProblems.forEach(function(dashProb){
+        if (problem._id === dashProb._id){
+          inDash = true;
+        }
+      });
+
+      return inDash;
+    };
+
+
     $scope.addProblem = function(problem){
-      ProblemData.addOwn(problem)
+      ProblemData.addOwn(problem);
+      ProblemData.getOwn()
+      .then(function(problems){
+        $scope.dashboardProblems = problems;
+      });
     };
 
   });
