7209a3fd7a5d89320434f78c6d546514bbe4045a
Mark Macdonald <mjemacdonald@gmail.com>
2015-03-23 09:50:06 -0700

    (feat) Applies basic layout for library view and controller

--- client/app/components/library/libraryController.js
+++ client/app/components/library/libraryController.js
@@ -1,13 +1,34 @@
 angular.module('RecallJS')
-  .controller('LibraryController', LibraryController);
+  .controller('LibraryController', function($window, $scope, Auth, ProblemData, LearningAlgo){
 
-function LibraryController($scope, ProblemData){
-  ProblemData.getAll()
-    .then(function(problems){
-      $scope.problems = problems;
-    });
+    ProblemData.getOwn()
+      .then(function(problems){
+        $scope.problems = problems;
+      });
 
-  $scope.add = function(problem){
-    ProblemData.addOwn(problem);
-  };
-}
+    $scope.remove = function(problem){
+      ProblemData.removeOwn(problem)
+        .then(function(){
+          ProblemData.getOwn()
+            .then(function(problems){
+              $scope.problems = problems;
+            });
+        });
+    };
+
+    $scope.username = "TODO: Replace with actual username";
+    $scope.calcWeight = LearningAlgo.calculateWeight;
+    $scope.lastAttempted = LearningAlgo.getLastAttemptDate;
+    $scope.avgRating = LearningAlgo.calcAverageRating;
+    console.log($scope.problems);
+
+    // Requirements
+    //   1. Use ProblemData.getOwn() to obtain user's problems
+    //   2. Display user's problems in a table
+    //      a. Problem title
+    //      b. Problem prompt
+    //      c. # of attempts
+    //      d. Average rating
+    //      e. Last attempt
+    //      f. Associated weight (maybe)
+  });
